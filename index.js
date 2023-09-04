7c8e6841c5421cbd16be8b5fda9ecdc7de55a689
Matheus Maia <matheussantosmaia@gmail.com>
2023-09-04 14:11:32 -0300

    teste

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ venom
   });
 
 function start(client) {
-
   client.onMessage((message) => {
-    if (message.body === 'Oi' ) {
+    if (message.body == 'Oi') {
       client
         .sendText(message.from, 'Welcome Venom 🕷')
         .then((result) => {
@@ -24,34 +23,35 @@ function start(client) {
     }
   });
 
-  setInterval(()=>{
+  setInterval(() => {
 
-    const arrayChute = ["💣", "💣", "💣", "💣", "⭐️","💣", "💣", "💣", "💣", "⭐️","💣", "💣", "💣", "💣", "⭐️","💣", "💣", "💣", "💣", "⭐️","💣", "💣", "💣", "💣", "⭐️",];
+
+    const arrayChute = ["💣", "💣", "💣", "💣", "⭐️", "💣", "💣", "💣", "💣", "⭐️", "💣", "💣", "💣", "💣", "⭐️", "💣", "💣", "💣", "💣", "⭐️", "💣", "💣", "💣", "💣", "⭐️",];
 
     let currentArray = arrayChute.slice().sort(() => Math.random() - 0.5);
     let previousArray = [];
     do {
       currentArray = currentArray.sort(() => Math.random() - 0.5);
     } while (currentArray.toString() === previousArray.toString());
-  
+
     previousArray = currentArray.slice();
     const expiryTime = 2; // tempo de expiração em minutos
     const now = new Date();
     const expiry = new Date(now.getTime() + expiryTime * 60 * 1000);
-    const hours = String(expiry.getHours() ).padStart(2, '0');
+    const hours = String(expiry.getHours()).padStart(2, '0');
     const minutes = String(expiry.getMinutes()).padStart(2, '0');
 
     client
-      .sendText('120363169790103246@g.us', 
-      `
+      .sendText('120363152231861042@g.us',
+        `
       🎲 Entrada confirmada 🎲 🥇: Entrada 
-      
-      ${currentArray [0]}${currentArray [1]}${currentArray [2]}${currentArray [3]}${currentArray [4]}
-      ${currentArray [5]}${currentArray [6]}${currentArray [7]}${currentArray [8]}${currentArray [9]}
-      ${currentArray [10]}${currentArray [11]}${currentArray [12]}${currentArray [13]}${currentArray [14]}
-      ${currentArray [15]}${currentArray [16]}${currentArray [17]}${currentArray [18]}${currentArray [19]}
-      ${currentArray [20]}${currentArray [21]}${currentArray [22]}${currentArray [23]}${currentArray [24]}
-    
+  
+      ${currentArray[0]}${currentArray[1]}${currentArray[2]}${currentArray[3]}${currentArray[4]}
+      ${currentArray[5]}${currentArray[6]}${currentArray[7]}${currentArray[8]}${currentArray[9]}
+      ${currentArray[10]}${currentArray[11]}${currentArray[12]}${currentArray[13]}${currentArray[14]}
+      ${currentArray[15]}${currentArray[16]}${currentArray[17]}${currentArray[18]}${currentArray[19]}
+      ${currentArray[20]}${currentArray[21]}${currentArray[22]}${currentArray[23]}${currentArray[24]}
+  
       🎮: Tentativas: 2
       📲: Plataforma correta: https://track.luck.bet/visit/?bta=35057&brand=luckbet
       💥: 5 minas
@@ -59,12 +59,16 @@ function start(client) {
       ⏱️ Válido até : ${hours}:${minutes}
       `)
       .then((result) => {
+        console.log('Result: ', result); //return object success
       })
       .catch((erro) => {
         console.error('Error when sending: ', erro); //return object error
       });
-  },10000)
-
-
+  }, 300000)
 }
+
+
+
+
+
 
